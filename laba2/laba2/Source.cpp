28afbdb84a86d2ca696b99441950d8c75a92058d
Nemec25 <rudalack@yandex.ru>
2016-06-10 13:46:32 +0600

    Create printf and scanf

--- laba2/laba2/Source.cpp
+++ laba2/laba2/Source.cpp
@@ -6,6 +6,12 @@ float a, b, c, d, x1, x2;
 int main()
 {
 	setlocale(LC_ALL, "Rus");
+	printf("¬ведите a ");
+	scanf_s("%g", &a);
+	printf("\n¬ведите b ");
+	scanf_s("%g", &b);
+	printf("\n¬ведите c ");
+	scanf_s("%g", &c);
 	_getch();
 	return 0;
 }
